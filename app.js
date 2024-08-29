4530150597aa1c1d58d342740d05837bb596f13c
Mike Belyayev <4xgood@gmail.com>
2024-08-28 18:54:32 -0600

    added query params

--- app.js
+++ app.js
@@ -15,12 +15,60 @@ app.get("/listings", async (req, res) => {
     // Connect to the database
     let pool = await sql.connect(config);
 
-    // Query the database
-    let result = await pool
-      .request()
-      .query(
-        "SELECT * FROM Properties WHERE Address_City = 'Calgary' AND YearBuilt = 2007"
-      );
+    // Base query
+    let query = "SELECT * FROM Properties WHERE 1=1";
+    const params = [];
+
+    // Add filters based on query parameters
+    if (req.query.SubType) {
+      query += " AND SubType = @SubType";
+      params.push({
+        name: "SubType",
+        type: sql.VarChar,
+        value: req.query.SubType,
+      });
+    }
+    if (req.query.ListPrice) {
+      query += " AND ListPrice <= @ListPrice";
+      params.push({
+        name: "ListPrice",
+        type: sql.Decimal,
+        value: req.query.ListPrice,
+      });
+    }
+    if (req.query.YearBuilt) {
+      query += " AND YearBuilt = @YearBuilt";
+      params.push({
+        name: "YearBuilt",
+        type: sql.Int,
+        value: req.query.YearBuilt,
+      });
+    }
+    if (req.query.Address_City) {
+      query += " AND Address_City = @Address_City";
+      params.push({
+        name: "Address_City",
+        type: sql.VarChar,
+        value: req.query.Address_City,
+      });
+    }
+    if (req.query.ArchitecturalStyle) {
+      query += " AND ArchitecturalStyle = @ArchitecturalStyle";
+      params.push({
+        name: "ArchitecturalStyle",
+        type: sql.VarChar,
+        value: req.query.ArchitecturalStyle,
+      });
+    }
+
+    // Create request and add parameters
+    let request = pool.request();
+    params.forEach((param) => {
+      request.input(param.name, param.type, param.value);
+    });
+
+    // Execute query
+    let result = await request.query(query);
 
     // Send the result as a response
     res.json(result.recordset);
